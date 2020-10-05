72cc9ad2e812282f328d6848c4f1b9a5f99cf2ad
thanh phu <htp1912@gmail.com>
2020-10-05 16:55:44 +0700

    Update config

--- config.go
+++ config.go
@@ -6,4 +6,11 @@ type Config struct {
 	Level           string           `mapstructure:"level"`
 	TimestampFormat string           `mapstructure:"timestamp_format"`
 	FieldMap        *logrus.FieldMap `mapstructure:"field_map"`
+	Duration        string           `mapstructure:"duration"`
+	Fields          string           `mapstructure:"fields"`
+}
+
+type FieldConfig struct {
+	Duration string    `mapstructure:"duration"`
+	Fields   *[]string `mapstructure:"fields"`
 }
--- logrus.go
+++ logrus.go
@@ -1,41 +1,131 @@
 package log
 
-import "github.com/sirupsen/logrus"
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"strings"
+	"time"
+)
+
+var fieldConfig FieldConfig
+
+const FIELDS = "logFields"
 
 func Initialize(c Config) *logrus.Logger {
-	logger := logrus.New()
-	if len(c.Level) > 0 {
-		if level, err := logrus.ParseLevel(c.Level); err == nil {
-			logger.SetLevel(level)
-		} else {
-			logrus.Errorf("Can't parse LOG_LEVEL: %s.", c.Level)
-		}
+	if len(c.Duration) > 0 {
+		fieldConfig.Duration = c.Duration
+	} else {
+		fieldConfig.Duration = "duration"
 	}
-	// MiddleWareLog as JSON instead of the default ASCII formatter.
-	// logger.SetFormatter(&logrus.JSONFormatter{})
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	// logger.SetOutput(os.Stdout)
+	if len(c.Fields) > 0 {
+		fields := strings.Split(c.Fields, ",")
+		fieldConfig.Fields = &fields
+	}
+	logger := logrus.New()
 
 	// kibana: time:@timestamp msg:message
-	formatter := logrus.JSONFormatter{
-		// disable, as we set our own
-		TimestampFormat: "2006-01-02T15:04:05.000Z0700",
-		/*
-			DisableTimestamp: false,
-			FieldMap: logrus.FieldMap{
-				logrus.FieldKeyTime:  "@timestamp",
-				logrus.FieldKeyLevel: "@level",
-				logrus.FieldKeyMsg:   "@message",
-				logrus.FieldKeyFunc:  "@caller",
-			}, */
-	}
+	formatter := logrus.JSONFormatter{TimestampFormat: "2006-01-02T15:04:05.000Z0700"}
 	if len(c.TimestampFormat) > 0 {
 		formatter.TimestampFormat = c.TimestampFormat
 	}
 	if c.FieldMap != nil {
 		formatter.FieldMap = *c.FieldMap
 	}
-	logger.SetFormatter(&formatter)
+	x := &formatter
+	logger.SetFormatter(x)
+	logrus.SetFormatter(x)
+	if len(c.Level) > 0 {
+		if level, err := logrus.ParseLevel(c.Level); err == nil {
+			logger.SetLevel(level)
+			logrus.SetLevel(level)
+		} else {
+			logrus.Errorf("Can't parse LOG_LEVEL: %s.", c.Level)
+		}
+	}
 	return logger
+}
+
+func LogDuration(ctx context.Context, level logrus.Level, start time.Time, args ...interface{}) {
+	end := time.Now()
+	duration := end.Sub(start)
+	fields := AppendFields(ctx, logrus.Fields{})
+	fields[fieldConfig.Duration] = duration.Milliseconds()
+	logrus.WithFields(fields).Log(level, args...)
+}
+func LogfDuration(ctx context.Context, level logrus.Level, start time.Time, format string, args ...interface{}) {
+	end := time.Now()
+	duration := end.Sub(start)
+	fields := AppendFields(ctx, logrus.Fields{})
+	fields[fieldConfig.Duration] = duration.Milliseconds()
+	logrus.WithFields(fields).Logf(level, format, args...)
+}
+
+func Log(ctx context.Context, level logrus.Level, args ...interface{}) {
+	fields := AppendFields(ctx, logrus.Fields{})
+	logrus.WithFields(fields).Log(level, args...)
+}
+func Logf(ctx context.Context, level logrus.Level, format string, args ...interface{}) {
+	fields := AppendFields(ctx, logrus.Fields{})
+	logrus.WithFields(fields).Logf(level, format, args...)
+}
+
+func AppendFields(ctx context.Context, fields logrus.Fields) logrus.Fields {
+	if logFields, ok := ctx.Value(FIELDS).(map[string]interface{}); ok {
+		for k, v := range logFields {
+			fields[k] = v
+		}
+	}
+	if fieldConfig.Fields != nil {
+		cfs := *fieldConfig.Fields
+		for _, k2 := range cfs {
+			if v2, ok := ctx.Value(k2).(string); ok && len(v2) > 0 {
+				fields[k2] = v2
+			}
+		}
+	}
+	return fields
+}
+
+func Panic(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.PanicLevel, args...)
+}
+func Fatal(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.FatalLevel, args...)
+}
+func Error(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.ErrorLevel, args...)
+}
+func Warn(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.WarnLevel, args...)
+}
+func Info(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.InfoLevel, args...)
+}
+func Debug(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.DebugLevel, args...)
+}
+func Trace(ctx context.Context, args ...interface{}) {
+	Log(ctx, logrus.TraceLevel, args...)
+}
+
+func Panicf(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.PanicLevel, format, args...)
+}
+func Fatalf(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.FatalLevel, format, args...)
+}
+func Errorf(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.ErrorLevel, format, args...)
+}
+func Warnf(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.WarnLevel, format, args...)
+}
+func Infof(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.InfoLevel, format, args...)
+}
+func Debugf(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.DebugLevel, format, args...)
+}
+func Tracef(ctx context.Context, format string, args ...interface{}) {
+	Logf(ctx, logrus.TraceLevel, format, args...)
 }
