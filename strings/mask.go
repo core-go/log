a5e8c2d192c01700a63cc2caf29ed12987d00627
Duc Nguyen <minhduc140583@gmail.com>
2021-06-06 18:15:27 +0700

    Separate Mask into package strings

---
+++ strings/mask.go
@@ -0,0 +1,35 @@
+package strings
+
+import "strings"
+
+func Mask(s string, start int, end int, mask string) string {
+	if start < 0 {
+		start = 0
+	}
+	if end < 0 {
+		end = 0
+	}
+	if start+end >= len(s) {
+		return strings.Repeat(mask, len(s))
+	}
+	return s[:start] + strings.Repeat(mask, len(s)-start-end) + s[len(s)-end:]
+}
+
+func MaskMargin(s string, start int, end int, mask string) string {
+	if start >= end {
+		return ""
+	}
+	if start < 0 {
+		start = 0
+	}
+	if end < 0 {
+		end = 0
+	}
+	if start >= len(s) {
+		return strings.Repeat(mask, len(s))
+	}
+	if end >= len(s) {
+		return strings.Repeat(mask, start) + s[start:]
+	}
+	return strings.Repeat(mask, start) + s[start:end] + strings.Repeat(mask, len(s)-end)
+}
