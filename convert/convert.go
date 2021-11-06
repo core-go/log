6b374990fd826983f8435905ab8c8a133b695488
Duc Nguyen <minhduc140583@gmail.com>
2021-11-06 20:02:06 +0700

    Add convert to camel case

---
+++ convert/convert.go
@@ -0,0 +1,25 @@
+package convert
+
+import "strings"
+
+func ToCamelCase(m map[string]string) map[string]string {
+	p := make(map[string]string)
+	for key, element := range m {
+		n := ToCamel(key)
+		p[n] = element
+	}
+	return p
+}
+func ToCamel(s string) string {
+	s2 := strings.ToUpper(s)
+	s1 := string(s[0])
+	for i := 1; i < len(s); i++ {
+		if string(s[i-1]) == "_" {
+			s1 = s1[:len(s1)-1]
+			s1 += string(s2[i])
+		} else {
+			s1 += string(s[i])
+		}
+	}
+	return s1
+}
